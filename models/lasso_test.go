3517558eb06cc3c2592e94269755b640fd5782a0
Austin Ouyang <aouyang1@gmail.com>
2024-12-25 12:14:38 -0600

    added auto lasso to pick best lambda regularization parameter

--- models/lasso_test.go
+++ models/lasso_test.go
@@ -123,6 +123,78 @@ func TestLassoRegression(t *testing.T) {
 	assert.InDelta(t, 1.0, r2, 0.00001)
 }
 
+func TestLassoAutoRegression(t *testing.T) {
+	// y = 2 + 3*x0 + 4*x1
+	x, err := mat_.NewDenseFromArray(
+		[][]float64{
+			{0, 0},
+			{3, 5},
+			{9, 20},
+			{12, 6},
+		},
+	)
+	require.Nil(t, err)
+
+	y := mat.NewDense(4, 1, []float64{2, 31, 109, 62})
+
+	opt := new(LassoAutoOptions)
+	opt.Lambdas = []float64{0.0, 1.0, 10.0, 100.0, 1000.0, 10000.0}
+	opt.Tolerance = 1e-6
+	opt.Iterations = 500
+	opt.FitIntercept = true
+	opt.Parallelization = 4
+
+	model, err := NewLassoAutoRegression(opt)
+	require.Nil(t, err)
+
+	err = model.Fit(x, y)
+	require.Nil(t, err)
+
+	assert.InDelta(t, 2.0, model.Intercept(), 0.00001)
+
+	coef := model.Coef()
+	assert.InDelta(t, 3.0, coef[0], 0.00001)
+	assert.InDelta(t, 4.0, coef[1], 0.00001)
+
+	r2, err := model.Score(x, y)
+	require.Nil(t, err)
+	assert.InDelta(t, 1.0, r2, 0.00001)
+
+	x, err = mat_.NewDenseFromArray(
+		[][]float64{
+			{1, 0, 0},
+			{1, 3, 5},
+			{1, 9, 20},
+			{1, 12, 6},
+		},
+	)
+	require.Nil(t, err)
+
+	y = mat.NewDense(4, 1, []float64{2, 31, 109, 62})
+
+	opt = new(LassoAutoOptions)
+	opt.Lambdas = []float64{0.0, 1.0, 10.0, 100.0, 1000.0, 10000.0}
+	opt.Tolerance = 1e-6
+	opt.Iterations = 500
+	opt.FitIntercept = false
+	opt.Parallelization = 4
+
+	model, err = NewLassoAutoRegression(opt)
+	require.Nil(t, err)
+
+	err = model.Fit(x, y)
+	require.Nil(t, err)
+
+	coef = model.Coef()
+	assert.InDelta(t, 2.0, coef[0], 0.00001)
+	assert.InDelta(t, 3.0, coef[1], 0.00001)
+	assert.InDelta(t, 4.0, coef[2], 0.00001)
+
+	r2, err = model.Score(x, y)
+	require.Nil(t, err)
+	assert.InDelta(t, 1.0, r2, 0.00001)
+}
+
 func BenchmarkLassoRegression(b *testing.B) {
 	nObs := 1000
 	nFeat := 100
