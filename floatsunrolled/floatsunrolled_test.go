646ebf1edcf3bef1065353f55f92c959ecce8a02
Austin Ouyang <aouyang1@gmail.com>
2025-01-12 20:48:57 -0800

    added unit tests to floatsunrolled

---
+++ floatsunrolled/floatsunrolled_test.go
@@ -0,0 +1,203 @@
+package floatsunrolled
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDot(t *testing.T) {
+	testData := map[string]struct {
+		a        []float64
+		b        []float64
+		err      error
+		expected float64
+	}{
+		"length mismatch": {
+			a:   []float64{1, 2, 3},
+			b:   []float64{1, 2},
+			err: ErrSliceLengthMismatch,
+		},
+		"length multiple invalid": {
+			a:   []float64{1, 2, 3},
+			b:   []float64{1, 2, 3},
+			err: ErrSliceMul,
+		},
+		"valid": {
+			a:        []float64{1, 2, 3, 4},
+			b:        []float64{4, 3, 2, 1},
+			expected: 20,
+		},
+	}
+
+	for name, td := range testData {
+		t.Run(name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					if td.err != nil {
+						err, ok := r.(error)
+						assert.True(t, ok)
+						assert.EqualError(t, err, td.err.Error())
+						return
+					}
+
+					assert.Nil(t, r)
+				}
+			}()
+			res := Dot(td.a, td.b)
+			assert.Equal(t, td.expected, res)
+		})
+	}
+}
+
+func TestAdd(t *testing.T) {
+	testData := map[string]struct {
+		a        []float64
+		b        []float64
+		err      error
+		expected []float64
+	}{
+		"length mismatch": {
+			a:   []float64{1, 2, 3},
+			b:   []float64{1, 2},
+			err: ErrSliceLengthMismatch,
+		},
+		"length multiple invalid": {
+			a:   []float64{1, 2, 3},
+			b:   []float64{1, 2, 3},
+			err: ErrSliceMul,
+		},
+		"valid": {
+			a:        []float64{1, 2, 3, 4},
+			b:        []float64{4, 3, 2, 1},
+			expected: []float64{5, 5, 5, 5},
+		},
+	}
+
+	for name, td := range testData {
+		t.Run(name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					if td.err != nil {
+						err, ok := r.(error)
+						assert.True(t, ok)
+						assert.EqualError(t, err, td.err.Error())
+						return
+					}
+
+					assert.Nil(t, r)
+				}
+			}()
+			res := Add(td.a, td.b)
+			assert.Equal(t, td.expected, res)
+		})
+	}
+}
+
+func TestSubTo(t *testing.T) {
+	testData := map[string]struct {
+		dst      []float64
+		s        []float64
+		t        []float64
+		err      error
+		expected []float64
+	}{
+		"length mismatch": {
+			s:   []float64{1, 2, 3},
+			t:   []float64{1, 2},
+			err: ErrSliceLengthMismatch,
+		},
+		"length multiple invalid": {
+			s:   []float64{1, 2, 3},
+			t:   []float64{1, 2, 3},
+			err: ErrSliceMul,
+		},
+		"valid no destination": {
+			s:        []float64{1, 2, 3, 4},
+			t:        []float64{4, 3, 2, 1},
+			expected: []float64{-3, -1, 1, 3},
+		},
+		"valid with destination": {
+			dst:      make([]float64, 4),
+			s:        []float64{1, 2, 3, 4},
+			t:        []float64{4, 3, 2, 1},
+			expected: []float64{-3, -1, 1, 3},
+		},
+		"invalid destination": {
+			dst: make([]float64, 3),
+			s:   []float64{1, 2, 3, 4},
+			t:   []float64{4, 3, 2, 1},
+			err: ErrOutputSliceLengthMismatch,
+		},
+	}
+
+	for name, td := range testData {
+		t.Run(name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					if td.err != nil {
+						err, ok := r.(error)
+						assert.True(t, ok)
+						assert.EqualError(t, err, td.err.Error())
+						return
+					}
+
+					assert.Nil(t, r)
+				}
+			}()
+			res := SubTo(td.dst, td.s, td.t)
+			assert.Equal(t, td.expected, res)
+		})
+	}
+}
+
+func TestScaleTo(t *testing.T) {
+	testData := map[string]struct {
+		dst      []float64
+		c        float64
+		s        []float64
+		err      error
+		expected []float64
+	}{
+		"length multiple invalid": {
+			s:   []float64{1, 2, 3},
+			err: ErrSliceMul,
+		},
+		"valid no destination": {
+			c:        3,
+			s:        []float64{1, 2, 3, 4},
+			expected: []float64{3, 6, 9, 12},
+		},
+		"valid with destination": {
+			dst:      make([]float64, 4),
+			c:        3,
+			s:        []float64{1, 2, 3, 4},
+			expected: []float64{3, 6, 9, 12},
+		},
+		"invalid destination": {
+			dst: make([]float64, 3),
+			c:   3,
+			s:   []float64{1, 2, 3, 4},
+			err: ErrOutputSliceLengthMismatch,
+		},
+	}
+
+	for name, td := range testData {
+		t.Run(name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					if td.err != nil {
+						err, ok := r.(error)
+						assert.True(t, ok)
+						assert.EqualError(t, err, td.err.Error())
+						return
+					}
+
+					assert.Nil(t, r)
+				}
+			}()
+			res := ScaleTo(td.dst, td.c, td.s)
+			assert.Equal(t, td.expected, res)
+		})
+	}
+}
